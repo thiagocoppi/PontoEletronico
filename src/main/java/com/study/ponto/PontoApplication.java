35c29f1136d59c7fcf81528dcb5b056c30fe38a0
Thiago Coppi <thiago.coppi@senior.com.br>
2018-03-02 09:51:50 -0300

    [add] initial commit

---
+++ src/main/java/com/study/ponto/PontoApplication.java
@@ -0,0 +1,12 @@
+package com.study.ponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoApplication.class, args);
+	}
+}
